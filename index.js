b2c675266bde9710167f5a70d066db61b4eda770
Daria Nikulina <daryanickulina@yandex.ru>
2023-06-08 20:03:20 +0400

    feat: adds 2 function

--- index.js
+++ index.js
@@ -26,3 +26,35 @@ Object.defineProperty(Array.prototype, "customFilter", {
     return result;
   },
 });
+
+function bubbleSort(arr) {
+  let newArr = [];
+
+  if (!arr || !Array.isArray(arr)) {
+    throw new Error("Invalid argument");
+  }
+
+  const isInvalid = arr.some((el) => {
+    return !isFinite(el) || typeof el !== "number";
+  });
+
+  if (isInvalid) {
+    throw new Error("Invalid argument");
+  }
+
+  newArr = arr.slice();
+
+  if (newArr.length < 1) {
+    return newArr;
+  }
+
+  for (let i = newArr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (newArr[j] > newArr[j + 1]) {
+        [newArr[j], newArr[j + 1]] = [newArr[j + 1], newArr[j]];
+      }
+    }
+  }
+
+  return newArr;
+}
